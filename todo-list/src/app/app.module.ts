294aba0a8eb03ae399554011697bfdb61e948b62
mateusz.jama <mateusz.jama@codefusion.pl>
2023-06-26 10:21:48 +0200

    change styles, extract logic for item to new component

--- todo-list/src/app/app.module.ts
+++ todo-list/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { FormsModule } from '@angular/forms';
+import { TodoItemComponent } from './todo-list/todo-item/todo-item.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoListComponent],
+  declarations: [AppComponent, TodoListComponent, TodoItemComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
